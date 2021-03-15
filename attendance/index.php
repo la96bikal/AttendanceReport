659ce642cbba7d9e88c2401ebbdb1fcccf5ca3fb
Bikal Lamichhane <35822085+la96bikal@users.noreply.github.com>
2021-03-15 01:10:35 -0400

    Add files via upload

--- attendance/index.php
+++ attendance/index.php
@@ -1,12 +1,10 @@
 <?php 	
 	require('DataAccesslayer/DAL.php');
 	require('DataAccesslayer/config/db.php');
-
-	$names = getDistinctNames(getConnectionString());	
-	$dates = getDistinctDates(getConnectionString());
-	$attendanceInfo = getAttendanceInfo(getConnectionString());
+	
+	$courses = getCourseIDs(getConnectionString());
+	
 ?>
-
 <html>
 <head>
 	<title> Attendance Report </title>
@@ -17,46 +15,37 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
 </head>
 
+<script>
+       
+</script> 
+
 <body>  
            <br /><br />  
            <div class="container">  
-                <h3 align="center">Attendance Report for CSC2515</h3>  
+                <h3 align="center">Attendance Report for 
+                	 <! -- The dropdown option for Crouse IDs -->            
+                	 <form action="" method="post" name = "courseForm">
+				
+                	 <select name = "formCourse">
+                	 	<option value = ""></option>
+                	<?php 
+                		foreach($courses as $course){
+                			echo '
+                			<option value ="'.$course.'">'.$course.'</option>
+                			';                			
+                		}
+                	?>
+                	</select>
+
+                	<input name = "submit" type = "submit" value = "Go">
+                	</form>
+
+               
+                </h3>  
                 <br />  
-                <div class="table-responsive">  
-                     <table id="Attendance_Report" class="table table-striped table-bordered">  
-                          <thead>  
-                          		<tr>
-                          			<td> Users </td>
 
-	                          		<?php
-	                          		foreach($dates as $date){
-	                          			echo '
-	                          			<td>'.$date.'</td>
-	                          			';
-	                          		}
-	                          		?>
-                               </tr>
-                          </thead>                                
-                          			<?php
-	                          		foreach($names as $name){
-	                          			echo '<tr>';	                          			
-	                          			echo '<td>'.$name.'</td>';
-	                          			foreach($dates as $date){
-	                          				// echo $date;	                          					                          				
-	                          				if(array_key_exists($date, $attendanceInfo[$name])){
-	                          					echo '<td>'.$attendanceInfo[$name][$date].'</td>';
-	                          				}
-	                          				else{
-	                          					echo '<td>'.'0'.'</td>';
-	                          				}
-
-	                          			}
-	                          			echo '</tr>';
-	                          		}
-	                          		?>                    
-                       
-                     </table>  
-                </div>  
+                <?php generateTable() ?>
+                
            </div>  
       </body>  
 
@@ -66,4 +55,66 @@
  $(document).ready(function(){  
       $('#Attendance_Report').DataTable();  
  });  
- </script>  
+
+ var auto_refresh = setInterval(function() { submitform(); }, 10000);
+ 	function submitform()
+					{
+  						document.getElementById("courseForm").submit();  									
+					}         
+
+</script>
+
+
+ <?php
+
+function generateTable(){
+		$dates = [];
+		if(isset($_POST['submit'])){    			
+            $courseID = $_POST['formCourse'];               
+            $dates = getDistinctDates(getConnectionString(), $courseID);  
+        	$names = getDistinctNames(getConnectionString(), $courseID);        	
+        	$attendanceInfo = getAttendanceInfo(getConnectionString());
+        
+
+		echo '
+		<h1 align = "center"> Atteandace report for '.$courseID.' </h3>
+		<div class="table-responsive">  
+                     <table id="Attendance_Report" class="table table-striped table-bordered">  
+                          <thead>  
+                          		<tr>
+                          			<td> Users </td>';                         		                          				
+          foreach($dates as $date){
+            echo '
+            <td>'.$date.'</td>
+            ';
+            }     
+                          			                          		
+           echo '</tr>
+                          </thead>'; 
+
+                          		foreach($names as $name){
+	            echo '<tr>';	                          			
+	            echo '<td>'.$name.'</td>';	           
+	            foreach($dates as $date){
+	                          				// echo $date;	                          					                          				
+	            if(array_key_exists($date, $attendanceInfo[$name])){
+	                echo '<td>'.$attendanceInfo[$name][$date].'</td>';
+	            }
+	            else{
+	                echo '<td>'.'0'.'</td>';
+	            }
+
+	            }
+	            echo '</tr>';
+	           }
+	        echo '                 
+                     </table>  
+                </div>  
+                ';
+            }
+	}
+?>
+
+
+
+  ?>
